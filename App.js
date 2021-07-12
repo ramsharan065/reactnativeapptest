1bb6a5713600d303246275b31ee7249d7573f889
ramsharan <ramsharan065@gmail.com>
2021-07-12 16:08:27 +0545

    load provided url in webview

---
+++ App.js
@@ -0,0 +1,96 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  Switch,
+  Button,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {WebView} from 'react-native-webview';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const [config, setConfig] = React.useState({"url":"", "isStorageEnabled":false});
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const [url, onUrlChange] = React.useState("");
+
+  const loadConfig = () => {
+    setConfig({"url":url, "isStorageEnabled": isEnabled});
+  }
+
+
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Url</Text>
+            <TextInput
+              multiline
+              style={styles.input}
+              onChangeText={onUrlChange}
+            />
+            <Text>domStorageEnabled</Text>
+            <Switch
+              style={styles.switch}
+              onValueChange={toggleSwitch}
+              value={isEnabled}  
+            />
+            <Button
+              title="load widget"
+              onPress={()=>loadConfig()}
+            />
+        </View>
+        <View style={styles.view}>
+          <WebView
+            incognito
+            // source={{html: html}}
+            source={{uri: config.url}}
+            originWhitelist={['*']}
+            javaScriptEnabled={true}
+            domStorageEnabled={config.isStorageEnabled}
+            startInLoadingState={true}
+          />
+        </View>
+    </SafeAreaView>
+
+  );
+  
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+  },
+  view:{
+    height:400
+  }
+});
+
+export default App;
